4cbba4f86eda7e756cfa69248c0cb5dc63157a73
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-09-01 15:26:02 -0300

    refactor: rename text component parameter and atribute

--- src/components/text.py
+++ src/components/text.py
@@ -3,10 +3,10 @@ import pygame
 class Text:
   """Text component"""
 
-  def __init__(self, position: tuple[float, float], surface: pygame.Surface, text_surface: pygame.Surface):
+  def __init__(self, position: tuple[float, float], screen_surface: pygame.Surface, text_surface: pygame.Surface):
     self.position = position
-    self.surface = surface
+    self.screen_surface = screen_surface
     self.text_surface = text_surface
 
   def process(self):
-    self.surface.blit(self.text_surface, self.position)
+    self.screen_surface.blit(self.text_surface, self.position)
