3b8510cde5ecef19cab5c89d79f67ae33ae67798
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-10-07 16:59:15 -0300

    feat: change colors and use the new ui.text component

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,7 @@
 import pygame
+
 from game import Game
+from ui import text
 
 def menu_scene(game: Game):
   for event in pygame.event.get():
@@ -10,11 +12,27 @@ def menu_scene(game: Game):
       if event.key == pygame.K_RETURN:
         game.scene = 1
 
-  game.screen.fill('white')
+  title = text.Text(
+    pygame.Vector2(
+      game.screen.get_width() / 2,
+      game.screen.get_height() / 4
+    ),
+    'Pyng Pong',
+    '#3776ab'
+  )
+  hit_enter = text.Text(
+    pygame.Vector2(
+      game.screen.get_width() / 2,
+      game.screen.get_height() / 2
+    ),
+    'Hit enter to start',
+    'white'
+  )
 
-  for component_name in game.ui_components['menu']:
-    component = game.ui_components['menu'][component_name]
-    component.process()
+  game.screen.fill('#141418')
+
+  title.process(game.screen, game.lg_text_font)
+  hit_enter.process(game.screen, game.md_text_font)
 
   pygame.display.flip()
   game.delta = game.clock.tick(60) / 1000
